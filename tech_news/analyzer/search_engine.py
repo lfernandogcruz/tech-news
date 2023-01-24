6d73dd4505b913447d2d35ce731638d391c325e8
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-24 18:48:13 -0300

    feat: req 07 - creates the function search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -15,7 +16,26 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        found = search_news(
+            {
+                "timestamp": {
+                    "$eq": datetime.fromisoformat(date).strftime("%d/%m/%Y")
+                }
+            }
+        )
+        list = []
+        for item in found:
+            list.append((item["title"], item["url"]))
+        return list
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
+# conversão de data de YYYY-MM-DD para DD/MM/YYYY sugerida em thread
+# no StackOverflow no link:
+# https://stackoverflow.com/questions/502726/converting-date-between-dd-mm-yyyy-and-yyyy-mm-dd
 
 
 # Requisito 8
